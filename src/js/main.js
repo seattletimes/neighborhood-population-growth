3c3bb1188431eb5d5ad74b91644094a5c28fa6a4
Alec Glassford <glassford@cs.stanford.edu>
2018-05-30 13:56:07 -0700

    New shapes for tracts (avoid water), adjust appearance, add absolute growth

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,13 @@ require("component-responsive-frame");
 // Percent with 1 decimal place
 var formatPercent = num => `${num.toFixed(1)}%`;
 
+// Doesn't work on 1 million+
+var formatWithComma = function addComma(n) {
+  var str = String(n);
+  if (str.length <= 3) return str;
+  return `${str.slice(0, -3)},${str.slice(-3)}`;
+};
+
 //get access to Leaflet and the map
 var element = document.querySelector("leaflet-map");
 var L = element.leaflet;
@@ -13,9 +20,11 @@ var map = element.map;
 var populationLayer = element.lookup["population-layer"];
 
 populationLayer.eachLayer((tractLayer) => {
-  var { populationGrowthPercent, tractNum } = tractLayer.feature.properties;
+  var { populationGrowthPercent, populationGrowthCount, tractNum } = tractLayer.feature.properties;
   var formattedGrowth = formatPercent(populationGrowthPercent);
-  tractLayer.bindPopup(`<h1 class="bigheader">Census tract ${tractNum}</h1> <b>Population growth:</b> ${formattedGrowth}`);
+  var formattedCount = formatWithComma(populationGrowthCount);
+  tractLayer.bindPopup(`<h1 class="bigheader">Census tract ${tractNum}</h1>
+<b>Population growth:</b> ${formattedGrowth} (+${formattedCount} people)`);
 });
 
 map.scrollWheelZoom.disable();
